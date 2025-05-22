f2e74f51e5b3212b82ed71e71f9c226415fc9257
Sangeerthan <sangeerthanetech@gmail.com>
2025-05-22 11:59:20 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
