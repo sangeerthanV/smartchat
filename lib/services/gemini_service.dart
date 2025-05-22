450563b6dcd82315a2896ff344bdaf4968d58f35
Sangeerthan <sangeerthanetech@gmail.com>
2025-05-22 12:18:08 +0530

    change the language api

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -8,7 +8,7 @@ final String apiKey = 'AIzaSyCH9jZpt5rCKQKZ6wkcnteK-tEH7vttdeU'; // Replace this
 
 Future<String> sendMessage(String message) async {
 final url = Uri.parse(
-'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$apiKey');
+'https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent=$apiKey');
 
 final response = await http.post(url,
 headers: {'Content-Type': 'application/json'},
