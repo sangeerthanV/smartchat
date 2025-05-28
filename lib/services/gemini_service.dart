9cc89bf96ce0bccb918d37c23fc4cda7ef6fecb0
Sangeerthan <sangeerthanetech@gmail.com>
2025-05-28 21:49:25 +0530

    change the gemini aoi key

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class GeminiService {
-  final String _apiKey ='AIzaSyCH9jZpt5rCKQKZ6wkcnteK-tEH7vttdeU';
+  final String _apiKey ='AIzaSyCAryYstoEg7wim4zx_bRTXeDx2bgukpek';
 
   Future<String> sendMessage(String userMessage) async {
-    const String baseUrl = 'curl -X GET '"https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyCH9jZpt5rCKQKZ6wkcnteK-tEH7vttdeU";
+    const String baseUrl = 'curl -X GET '"https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyCAryYstoEg7wim4zx_bRTXeDx2bgukpek";
     final Uri url = Uri.parse('$baseUrl?key=$_apiKey');
 
     final response = await http.post(
